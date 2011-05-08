39917e5691634f03661c670d1dcdbb294d059ff6
Ryan Burke <theryanburke@gmail.com>
2011-05-08 16:43:58 +0000

    adding skeleton files from rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Battlefork::Application.config.secret_token = 'b4cd662efcfe4190e52d7ff58b1b4703c48d9fb1be9b85c33d08e06f6e1172d34dc61676fb20ffab5fe092d4ef4baaecdcd3869869e648c1e50b3d2206ecfe0e'
